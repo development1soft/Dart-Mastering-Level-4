59d2a7ce8536494b8616c7a2ef6c1fd7a481cb9d
Eyad Web Developmnet <30596418+development1soft@users.noreply.github.com>
2020-06-26 00:24:02 +0300

    Base Project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_level_4/dart_level_4.dart' as dart_level_4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_level_4.calculate()}!');
+}
---
+++ test/dart_level_4_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_level_4/dart_level_4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
